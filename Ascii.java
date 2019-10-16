cf496a19bafead982350c5b0d7ee03c0066038b1
Anushaksasindran <anushaksasindran@gmail.com>
2019-10-16 17:19:32 +0530

    added

---
+++ Ascii.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Ascii {
+	public static void main(String[] args){
+		System.out.println("enter a string:");
+		Scanner scan = new Scanner(System.in);
+		  String s =scan.next();
+		char a = s.charAt(0);
+		System.out.println((int) a);
+		
+		
+	}
+
+}
