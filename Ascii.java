32b6e4c1b5c373b3c4d375b7bcabe925848ef059
Anushaksasindran <anushaksasindran@gmail.com>
2019-10-16 17:23:53 +0530

    Update Ascii.java

--- Ascii.java
+++ Ascii.java
@@ -1,9 +1,7 @@
 import java.util.*;
 public class Ascii {
 	public static void main(String[] args){
-		System.out.println("enter a string:");
-		Scanner scan = new Scanner(System.in);
-		  String s =scan.next();
+		String s;
 		char a = s.charAt(0);
 		System.out.println((int) a);
 		
